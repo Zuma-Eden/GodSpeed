ac3471191185757c868d32c7e5382560b2acfb1c
Darzuma <barryliu_3295@foxmail.com>
2022-02-16 02:19:47 +0800

    Initial commit

---
+++ dev/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <layout/>
+</template>
+
+<script>
+import layout from '@/components/layout'
+export default {
+    name:'App',
+    components:{
+        layout
+    }
+}
+</script>
+
